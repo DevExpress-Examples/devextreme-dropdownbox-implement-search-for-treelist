e95ded206427067db93f040d153149873eb65576
Diana Gromova <16adianay@gmail.com>
2026-05-14 17:20:32 +0400

    Update SampleDataController.cs

--- ASP.NET Core/Controllers/SampleDataController.cs
+++ ASP.NET Core/Controllers/SampleDataController.cs
@@ -15,7 +15,21 @@ public class SampleDataController: Controller {
     [HttpGet]
     [Route("GetTasks")]
     public object GetTasks(DataSourceLoadOptions loadOptions) {
-        return DataSourceLoader.Load(SampleData.EmployeeTasks, loadOptions);
+        var tasks = SampleData.EmployeeTasks;
+        var projected = tasks.Select(d => new EmployeeTask {
+            Task_ID = d.Task_ID,
+            Task_Parent_ID = d.Task_Parent_ID,
+            Task_Owner_ID = d.Task_Owner_ID,
+            Task_Assigned_Employee_ID = d.Task_Assigned_Employee_ID,
+            Task_Completion = d.Task_Completion,
+            Task_Priority = d.Task_Priority,
+            Task_Status = d.Task_Status,
+            Task_Subject = d.Task_Subject,
+            Task_Start_Date = d.Task_Start_Date,
+            Task_Due_Date = d.Task_Due_Date,
+            Has_Items = tasks.Any(t => t.Task_Parent_ID == d.Task_ID)
+        });
+        return DataSourceLoader.Load(projected, loadOptions);
     }
 
     [HttpGet]
